8cfb8d81071d223a5b03b49dea6b62f7fdd9e93a
malik <blue.bird@angelic.com>
2026-04-13 09:25:14 -0100

    Configure iOS/Windows Firebase initialization and workspace settings

---
+++ lib/firebase_options.dart
@@ -0,0 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
+
+class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform => const FirebaseOptions(
+    apiKey: 'AIzaSyAL5PbGAF9z-7C9DL01vTuz9ijEqMtyT60',
+    appId: '1:806998148004:ios:96e7e7ede187c75f933731',
+    messagingSenderId: '806998148004',
+    projectId: 'chessiq-89b45',
+    storageBucket: 'chessiq-89b45.firebasestorage.app',
+    authDomain: 'chessiq-89b45.firebaseapp.com',
+    databaseURL: 'https://chessiq-89b45.firebaseio.com',
+  );
+}
